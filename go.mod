ff36b8eae560bb2ae790ddeae8326de46ad65d6f
cayden <cuiran2001@163.com>
2020-08-19 16:46:46 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cayden/gomodone
+
+go 1.14
---
+++ say.go
@@ -0,0 +1,13 @@
+package gomodone
+
+import "fmt" 
+
+// say Hi to someone
+func SayHi(name string) string {
+   return fmt.Sprintf("Hi, %s", name)
+}
+
+// say Hi to someone
+func SayHello(name string) string {
+   return fmt.Sprintf("Hello, %s", name)
+}
